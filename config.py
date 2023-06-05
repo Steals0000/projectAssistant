b654b5198b679cde8e200a4e9785da9280561e0b
Vlad <www-game@mail.ru>
2023-06-05 15:09:46 +0300

    s2

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 import os
-
-import pandas as pd
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-import urllib
-
-from sqlalchemy import create_engine
-
-
 
 class Config:
     base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -16,10 +7,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret'
 
-
-
-
 app = Flask(__name__)
 app.config.from_object(Config())
-db = SQLAlchemy(app)
 
